bee4d32672ebc15184260a8ed82028553f6e73fc
DrunkOldDog <juanisoul@gmail.com>
2019-07-28 23:36:16 -0400

    Inicio Proyecto

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 22 },
+      { name: 'Emex', age: 47 },
+      { name: 'Pao', age: 21 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('clickeado');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working men</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          MyHobbies: Dota
+        </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'cumues\'a'));
+  }
+}
+
+export default App;
